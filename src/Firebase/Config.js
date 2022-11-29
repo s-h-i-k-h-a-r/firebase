52e0c9ee89e14d52f7627eb8020e9e346566844c
SizanRana <sizan.rana2000@gmail.com>
2022-11-29 22:37:24 +0545

    change firebase config files

--- src/Firebase/Config.js
+++ src/Firebase/Config.js
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app';
-
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCVtAlQRNNzZq5gkqdykUaVr6wSOdT4MWI",
-    authDomain: "antibug-720e1.firebaseapp.com",
-    projectId: "antibug-720e1",
-    storageBucket: "antibug-720e1.appspot.com",
-    messagingSenderId: "1055048748195",
-    appId: "1:1055048748195:web:d580c4972e7d8c2877c316"
+  apiKey: "AIzaSyCVtAlQRNNzZq5gkqdykUaVr6wSOdT4MWI",
+  authDomain: "antibug-720e1.firebaseapp.com",
+  projectId: "antibug-720e1",
+  storageBucket: "antibug-720e1.appspot.com",
+  messagingSenderId: "1055048748195",
+  appId: "1:1055048748195:web:d580c4972e7d8c2877c316",
 };
 
-const app = initializeApp(firebaseConfig);
-const firestore = app.firestore();
-export default {app , firestore}
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const firestore = getFirestore();
+export default { app, firestore };
